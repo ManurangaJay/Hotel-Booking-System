c3c5d6944a6373b4649ba6d82fb0250f34fa2e96
ManurangaJay <manurangajayawardhana@gmail.com>
2024-09-24 05:52:11 +0530

    Back end project was created with dependancies

---
+++ Cedarwood_Resort/src/test/java/com/ManuJay/Cedarwood_Resort/CedarwoodResortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ManuJay.Cedarwood_Resort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CedarwoodResortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
